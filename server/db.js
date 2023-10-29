5f5ed5b76cd834de28430002bc72fcfff22b67f7
giovanyosorio <giovany.torres.799@gmail.com>
2023-10-29 11:22:31 -0500

    project setup

--- server/db.js
+++ server/db.js
@@ -1,5 +1,9 @@
-import { Pool, createPool } from "mysql2/promise";
+import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-    
+    host: "localhost",
+    port:3306,
+    user: "root",
+    password: "gio123",
+    database: "tasksdb",
 })
