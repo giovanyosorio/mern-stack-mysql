f9a4f5ebf0c9d51fccb73e2e837b1db9d0629303
giovanyosorio <giovany.torres.799@gmail.com>
2023-11-04 21:29:51 -0500

    get task endpoint

--- server/controlers/task.controller.js
+++ server/controlers/task.controller.js
@@ -3,13 +3,19 @@ import {pool} from "../db.js"
 
 export const getTasks = async (req, res) => {
 
-    const result= await pool.query("SELECT * FROM tasks ORDER BY createdAT ASC")
-
+    const [result]= await pool.query("SELECT * FROM tasks ORDER BY createAt ASC")
+   // console.log(result);
+    res.json(result)
 }
 
-export const getTask =  (req, res) => {
+export const getTask = async (req, res) => {
 
-    res.send("Tasks Routes")
+    const [result]=await pool.query("SELECT * FROM tasks WHERE id=?",[req.params.id])
+    //console.log(result[0]);
+    if(result.length===0) return res.status(404).json({message:"Task not found"})
+    res.json(result[0])
+    
+    
 }
 
 
